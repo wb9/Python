669a45920fccf16a2cc18e757020665a43b448a4
wb9 <w_._b@wp.pl>
2019-09-06 00:14:53 +0200

    second

---
+++ Mars/druk_na_folii.py
@@ -0,0 +1,1 @@
+def druk_na_folii():
--- Mars/main.py
+++ Mars/main.py
@@ -1,19 +1,35 @@
 import baner
 import tabliczka_folia_PCV
 import koszulki
+#import druk_na_folii
+#import UV_PCV
 
-print ("Wybór: ")
-print("[1] BANERY")
-print("[2] TABLICZKA PCV + FOLIA")
-print("[3] KOSZULKI")
-print("[4]")
-print("[5]")
+def main():
 
-numer = float(input("Podaj numer: "))
+    print ("Wybór: ")
+    print("[1] BANERY")
+    print("[2] TABLICZKA PCV + FOLIA")
+    print("[3] KOSZULKI")
+    print("[4] DRUK NA FOLII")
+    print("[5] DRUK UV NA PCV")
 
-if numer == 1:
-    baner.baner()
-elif numer == 2:
-    tabliczka_folia_PCV.tabliczka()
-elif numer == 3:
-    koszulki.koszulki()
+    numer = float(input("Podaj numer: "))
+
+    if numer == 1:
+        baner.baner()
+    elif numer == 2:
+        tabliczka_folia_PCV.tabliczka()
+    elif numer == 3:
+        koszulki.koszulki()
+    #elif numer == 4:
+    #    druk_na_folii.druk_na_folii()
+    #elif numer == 5:
+    #    UV_PCV.uv_pcv()
+
+    koniec = input("Zacząć od nowa? T/N  ").lower()
+    if koniec == "t":
+        main()
+    else:
+        exit()
+
+main()
